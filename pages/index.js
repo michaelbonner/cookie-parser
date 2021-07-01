45de4918967b6bbbb88fdd89e177e257a9541c27
Michael Bonner <michaelwbonner@gmail.com>
2021-07-01 16:39:03 -0600

    initial parser commit

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,94 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import TextareaAutosize from "react-textarea-autosize";
 
 export default function Home() {
+  const [cookieString, setCookieString] = useState("");
+  const [cookieArray, setCookieArray] = useState([]);
+
+  useEffect(() => {
+    if (cookieString === "") {
+      setCookieArray([]);
+      return;
+    }
+
+    setCookieArray(
+      cookieString.split("; ").reduce((accumulator, current) => {
+        let [name, value] = current.split("=");
+        return [
+          ...accumulator,
+          {
+            name,
+            value: decodeURIComponent(value),
+          },
+        ];
+      }, [])
+    );
+  }, [cookieString]);
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Cookie Parser</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
+        <h1 className="text-6xl font-bold mt-12">Cookie Parser</h1>
+        <p className="mt-8">
+          Just drop in what you got from{" "}
+          <code className="bg-blue-100 py-1 px-2 rounded-md">
+            document.cookie
+          </code>{" "}
+          and we'll tell you what cookies are present.
         </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+        <TextareaAutosize
+          className="w-full border shadow-md border-gray-100 mt-8 py-4 px-8"
+          name="cookieString"
+          onChange={(event) => setCookieString(event.target.value)}
+          placeholder="Add the result from document.cookie from any site"
+          minRows="10"
+          value={cookieString}
+        />
+        {cookieArray.length ? (
+          <div className="border w-full max-w-4xl my-8 bg-gray-50 py-4 px-8">
+            {cookieArray.map((cookie) => {
+              return (
+                <div
+                  className="py-2 grid grid-cols-2 gap-x-4 text-lg"
+                  key={cookie.name}
+                >
+                  <dt className="text-xl text-right font-light">
+                    {cookie.name}
+                  </dt>
+                  <dd className="text-left font-medium">{cookie.value}</dd>
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+          <div
+            className="border w-full my-8 bg-gray-50 py-6 px-8"
+            style={{ minHeight: "300px" }}
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+            Enter your cookie string above
+          </div>
+        )}
       </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
+      <footer className="flex items-center justify-center w-full h-24">
         <a
           className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="https://bootpackdigital.com/"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
+          Powered by{" "}
+          <img
+            src="/bootpack-horizontal.svg"
+            alt="Bootpack Digital"
+            className="h-16 ml-2"
+          />
         </a>
       </footer>
     </div>
-  )
+  );
 }
