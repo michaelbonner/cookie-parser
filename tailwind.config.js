f9fe2ea5efa2982ee765d49db1be8127e3012529
Michael Bonner <michaelwbonner@gmail.com>
2022-02-01 23:22:15 -0700

    npm updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,13 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
     fontFamily: {
       sans: ["Inter", "sans-serif"],
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [require("@tailwindcss/forms")],
 };
