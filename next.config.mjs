435912202f4fba674d83f657bfadcf2d963272fa
Michael Bonner <michaelwbonner@gmail.com>
2024-07-02 16:43:56 -0600

    add i18n to next config

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  i18n: {
+    locales: ["en-US"],
+    defaultLocale: "en-US",
+  },
+};
+
+export default nextConfig;
